597d90969cf1cff161384a856156b488ed4df921
anilAmeotech <anil.ameotech@gmail.com>
2019-11-14 19:06:29 +0530

    6 files modified, 2 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { PlotBand } from '@progress/kendo-angular-charts';
+import { samplesGoals } from './data';
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
-})
-export class AppComponent  {
-  name = 'Angular';
+    selector: 'my-app',
+    templateUrl: './result.html'
+    })
+export class AppComponent {
+    plotList: any;
+
+     constructor() {
+     this.plotLists = samplesGoals;
+
+     console.log(this.plotList);
+    }
+
+    public labelContent = (e: any) => {
+    let reducedValue = "";
+    if(e.value >=0 && e.value <= 999)
+    {
+      reducedValue = e.value;  
+    }
+    else if (e.value >= 1000 && e.value <= 999999) { // thousands
+    reducedValue = (e.value / 1000) + "k";
+    } else if (e.value >= 1000000 && e.value <= 999999999) { // millions
+        reducedValue = (e.value / 1000000) + "m";
+    } else if (e.value >= 1000000000 && e.value <= 999999999999) { // billions
+        reducedValue = (e.value / 1000000000) + "b";
+    }
+
+    return reducedValue
 }
+ }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ChartsModule } from '@progress/kendo-angular-charts';
+import { HttpClientModule } from '@angular/common/http';
+
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [ BrowserModule, BrowserAnimationsModule, ChartsModule, FormsModule, HttpClientModule ],
+  declarations: [ AppComponent ],
+  bootstrap: [ AppComponent ]
 })
+
 export class AppModule { }
